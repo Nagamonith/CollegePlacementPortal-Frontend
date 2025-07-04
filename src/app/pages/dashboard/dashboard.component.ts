91c1dfe91f139ec4a3eb09fa3edf89461389faa4
Naga monith n <monith0404@gmail.com>
2025-07-04 15:30:04 +0530

    Added filter feature for eligible students and export to Excel

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterModule, Router } from '@angular/router'; // ✅ Add Router
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
+  constructor(private router: Router) {}
 
+  logout() {
+    // You can clear session or token here if needed
+    console.log("Logging out...");
+    this.router.navigate(['/']);
+  }
 }
